5aa1c485ce88c7a08e829e8fbf77337d73d982f1
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-12-01 11:01:04 +0100

    new polarization

--- scripts/run_comissioning_day8_and_9.py
+++ scripts/run_comissioning_day8_and_9.py
@@ -2,14 +2,14 @@ from mats_planningtool import configFile as configFile
 from mats_planningtool.XMLGenerator.XML_gen import XML_filter
     
 # #3130_POL
-# configfile = configFile.configFile("data/comissioning/configfile_3130_POL.json")
-# configfile.output_dir = "data/comissioning/"
-# configfile.CheckConfigFile()
+configfile = configFile.configFile("data/comissioning/configfile_3130_POL.json")
+configfile.output_dir = "data/comissioning/"
+configfile.CheckConfigFile()
 
-# configfile.Timeline_gen()
+#configfile.Timeline_gen()
 
-# configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3130_22120122113001POL.json")
-# XML_filter("data/comissioning/STP-MTS-3130_22120122113001POL.xml", "TC_acfLimbPointingAltitudeOffset")
+configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3130_22120122120102POL.json")
+#XML_filter("data/comissioning/STP-MTS-3130_22120122120102POL.xml", "TC_acfLimbPointingAltitudeOffset")
 
 # # #3103 Operational mode 1
 
@@ -23,15 +23,15 @@ from mats_planningtool.XMLGenerator.XML_gen import XML_filter
 
 
 
-# #3110_NADLMB
+# # #3110_NADLMB
 # configfile = configFile.configFile("data/comissioning/configfile_3110_NADLMB.json")
 # configfile.output_dir = "data/comissioning/"
 # configfile.CheckConfigFile()
 
-# #configfile.Timeline_gen()
+# # #configfile.Timeline_gen()
 
-# configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3110_22120222113001NADLMB.json")
-# XML_filter("data/comissioning/STP-MTS-3110_22120222113001NADLMB.xml", "TC_acfLimbPointingAltitudeOffset")
+# configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3110_22120222120102NADLMB.json")
+# XML_filter("data/comissioning/STP-MTS-3110_22120222120102NADLMB.xml", "TC_acfLimbPointingAltitudeOffset")
 
 # 3120 Crop-test
 
@@ -44,11 +44,11 @@ from mats_planningtool.XMLGenerator.XML_gen import XML_filter
 
 # #3103 Operational mode 1
 
-configfile = configFile.configFile("data/comissioning/configfile_3103_MODE1.json")
-configfile.output_dir = "data/comissioning/"
-configfile.CheckConfigFile()
+# configfile = configFile.configFile("data/comissioning/configfile_3103_MODE1.json")
+# configfile.output_dir = "data/comissioning/"
+# configfile.CheckConfigFile()
 
-configfile.Timeline_gen()
+# configfile.Timeline_gen()
 
-configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3103_22120122113004MODE1.json")
+# configfile.XML_gen("data/comissioning/Science_Mode_Timeline_3103_22120122113004MODE1.json")
 
